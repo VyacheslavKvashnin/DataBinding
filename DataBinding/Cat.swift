9599c66580b423f84442502cdcb04bd31abf6680
komplektan <komplektan@mail.ru>
2022-08-14 23:33:55 +0500

    Start

---
+++ DataBinding/Cat.swift
@@ -0,0 +1,14 @@
+//
+//  Cat.swift
+//  DataBinding
+//
+//  Created by Вячеслав Квашнин on 14.08.2022.
+//
+
+import Foundation
+
+struct Cat {
+    let name: String
+    let age: Int
+    let color: String
+}
